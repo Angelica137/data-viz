14c4470eb368a3e40a7c076f4dcbc79b67d3e2c3
Angelica <Angelica137@users.noreply.github.com>
2020-09-29 20:29:39 +0100

    scatter plot

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 
 
-input_values = [1, 2, 3, 4, 5]
-squares = [1, 4, 9, 16, 25]
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(2, 4, s=200)
+ax.scatter(x_values, y_values, s=100)
 
 # Set chart tltile and label axes
 ax.set_title("Square Numbers", fontsize=24)
